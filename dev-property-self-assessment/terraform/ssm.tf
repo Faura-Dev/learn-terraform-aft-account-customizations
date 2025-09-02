e1a27b044d8d79c3ed5e7a1468f6fef226080bae
Henry Wanjala <wanjala.henry.dev@gmail.com>
2025-09-02 14:25:16 -0700

    fix audit bucket naming

--- dev-property-self-assessment/terraform/ssm.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ssm_parameter" "foo" {
-  name = "/accID"
-  type = "String"
-  value = "${data.aws_caller_identity.current.account_id}"
-}
-
--- staging-property-self-assessment/terraform/s3.tf
+++ staging-property-self-assessment/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-staging-${data.aws_caller_identity.current.account_id}-audit-bucket"
+  bucket = "aft-staging-property-self-assessment-audit-bucket"
   acl    = "private"
 }
