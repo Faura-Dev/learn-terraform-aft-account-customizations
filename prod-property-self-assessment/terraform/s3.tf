2246d55b3e96e8c9938715b58e1c1347d5a5eeee
Henry Wanjala <wanjala.henry.dev@gmail.com>
2025-09-02 12:41:19 -0700

    add resilience model accounts

--- prod-property-self-assessment/terraform/s3.tf
+++ prod-property-self-assessment/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-production-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-production-${data.aws_caller_identity.current.account_id}-audit-bucket"
   acl    = "private"
 }
--- prod-resilience-model/terraform/s3.tf
+++ prod-resilience-model/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-dev-${data.aws_caller_identity.current.account_id}-audit-bucket"
+  bucket = "aft-prod-resilience-model-${data.aws_caller_identity.current.account_id}-audit-bucket"
   acl    = "private"
 }
